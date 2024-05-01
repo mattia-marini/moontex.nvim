07c2568ef5c54144efc7d9c8dc3f711d3186f78a
Mattia Marini <marmattia26@gmail.com>
2024-05-01 16:18:00 +0200

    Moved compiling status to buf variable

--- lua/MoonTex/compile.lua
+++ lua/MoonTex/compile.lua
@@ -1,33 +1,40 @@
 local config = require("MoonTex.config")
+local util = require("MoonTex.util")
 local tex_fs = require("MoonTex.fs")
 
 --starts/stops continuous conpile with latekmk
-local compile_running = false
+util.set_buf_status("compiling", false)
+
 function toggle_compile()
-  if compile_running == false then
-    compile_running = true
+  if util.get_buf_status("compiling") == false then
+    util.set_buf_status("compiling", true)
     vimtex_compile_start()
   else
-    compile_running = false
+    util.set_buf_status("compiling", false)
     vimtex_compile_stop()
   end
 end
 
 --starts continuous conpile with latekmk
 function vimtex_compile_start()
-  
-  if vim.api.nvim_buf_get_var(0,"main_file_dir")==nil then print("Coundn't resolve mainfile path") return end
+  local mainfile_dir = util.get_buf_status("mainfile_dir")
+  if mainfile_dir == nil then
+    print("Coundn't resolve mainfile path")
+    return
+  end
 
-  vim.api.nvim_echo({{"Compiling...", "Substitute"}}, true, {})
+  vim.api.nvim_echo({ { "Compiling...", "Substitute" } }, true, {})
   vim.cmd("silent w")
   --print("cd "..vim.api.nvim_buf_get_var(0,"main_file_dir").." && latexmk -interaction=nonstopmode -r "..vim.api.nvim_get_var("MoonTex_install_dir").."/.latexmkrc ".. config.main_file_name)
-  compile_channel_id=vim.fn.jobstart("cd \""..vim.api.nvim_buf_get_var(0,"main_file_dir").."\" && latexmk -interaction=nonstopmode -r "..vim.api.nvim_get_var("MoonTex_install_dir").."/.latexmkrc ".. config.main_file_name, {on_exit = function() vim.api.nvim_echo({{"Compiling stopped!", "Substitute"}}, true, {}) end})
-
+  compile_channel_id = vim.fn.jobstart(
+  "cd \"" ..
+  mainfile_dir ..
+  "\" && latexmk -interaction=nonstopmode -r " ..
+  vim.api.nvim_get_var("MoonTex_install_dir") .. "/.latexmkrc " .. config.main_file_name,
+  { on_exit = function() vim.api.nvim_echo({ { "Compiling stopped!", "Substitute" } }, true, {}) end })
 end
 
 --starts continuous conpile with latekmk
 function vimtex_compile_stop()
   vim.fn.jobstop(compile_channel_id)
 end
-
-
--- lua/MoonTex/util.lua
+++ lua/MoonTex/util.lua
@@ -1,10 +1,23 @@
-local function get_mainfile()
-
+local function get_buf_status(name)
   local found, rv = pcall(function()
-    return vim.api.nvim_buf_get_var(0, "mt_status").mainfile_dir
+    return vim.api.nvim_buf_get_var(0, "mt_status")[name]
   end
   )
 
   return (found and rv or nil)
-
 end
+
+local function set_buf_status(name, val)
+  local t = vim.api.nvim_buf_get_var(0, "mt_status")
+  table.insert(t, name, val)
+  vim.api.nvim_buf_set_var(0, "mt_status", t)
+end
+
+
+
+local rv = {
+  ["get_buf_status"] = get_buf_status,
+  ["set_buf_status"] = set_buf_status
+}
+
+return rv
